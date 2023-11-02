0732a55bc9b12841e8790ffeb312f0baedbedc53
Marc Liang Perarnau Olaya <145003837+MPeOl@users.noreply.github.com>
2023-11-02 11:44:46 +0100

    Update code.js

--- code.js
+++ code.js
@@ -1,9 +1,1 @@
-$('#Swatch1').click(function(){
-    document.querySelector('#Swatch1').classList.toggle('active');
-});
-$('#Swatch2').click(function(){
-    document.querySelector('#Swatch2').classList.toggle('active');
-});
-$('#Swatch3').click(function(){
-    document.querySelector('#Swatch3').classList.toggle('active');
-});
+
